095515a5c5c83f28f03af6582c22dac97e308741
Scott Rutz <scrutz@microsoft.com>
2020-07-08 14:39:18 -0500

    Added Filtering

--- Demo.EventProcessor/Demo.EventProcessor/EventProcessorActivity.cs
+++ Demo.EventProcessor/Demo.EventProcessor/EventProcessorActivity.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
 using Microsoft.Azure.WebJobs;
@@ -11,34 +12,20 @@ namespace Demo.EventProcessor
 {
     public static class EventProcessorActivity
     {
-        [FunctionName("EventProcessorActivity")]
-        public static async Task Run([EventHubTrigger("ingest-001", Connection = "IngestEventHubConnectionString")] EventData[] events, ILogger log)
-        {
-            var exceptions = new List<Exception>();
+        private static List<string> ignoreTags = new List<string>() { "Tag5", "Tag6" };
 
+        [FunctionName("EventProcessorActivity")]
+        public static async Task Run([EventHubTrigger("ingest-001", Connection = "IngestEventHubConnectionString")] EventData[] events,
+                                     [EventHub("output-001", Connection = "OutputEventHubConnectionString")] IAsyncCollector<string> outputEvents, ILogger log)
+        {
             log.LogMetric("EventProcessorActivityBatchSize", events.Count(), new Dictionary<string, object> { { "RunId", Guid.NewGuid()} });
             foreach (EventData eventData in events)
             {
-                try
-                {
-                    string messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
-                    await Task.Yield();
-                }
-                catch (Exception e)
-                {
-                    // We need to keep processing the rest of the batch - capture this exception and continue.
-                    // Also, consider capturing details of the message that failed processing so it can be processed again later.
-                    exceptions.Add(e);
-                }
+                var messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
+                var telemetryModel = JsonSerializer.Deserialize<TelemetryModel>(messageBody);
+                var telemetryModelFiltered = new TelemetryModel() { Data = telemetryModel.Data.Where(t => !ignoreTags.Any(i => i == t.Name)) };
+                await outputEvents.AddAsync(JsonSerializer.Serialize(telemetryModelFiltered));
             }
-
-            // Once processing of the batch is complete, if any messages in the batch failed processing throw an exception so that there is a record of the failure.
-
-            if (exceptions.Count > 1)
-                throw new AggregateException(exceptions);
-
-            if (exceptions.Count == 1)
-                throw exceptions.Single();
         }
     }
 }
