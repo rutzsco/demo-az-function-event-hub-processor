95b645a0aa807f17571dcdd3afc141f1c5be3f76
Scott Rutz <scrutz@microsoft.com>
2020-07-13 10:43:22 -0500

    CP

---
+++ Demo.EventProcessor/Demo.EventProcessor/SendEventHubMessageEndpoint.cs
@@ -0,0 +1,27 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace Demo.EventProcessor
+{
+    public static class SendEventHubMessageEndpoint
+    {
+        [FunctionName("SendEventHubMessageEndpoint")]
+        public static async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post", Route = null)] HttpRequest req, 
+            [EventHub("output-001", Connection = "OutputEventHubConnectionString")] IAsyncCollector<string> outputEvents, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+
+            string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            await outputEvents.AddAsync(requestBody);
+            return new OkObjectResult("OK");
+        }
+    }
+}
