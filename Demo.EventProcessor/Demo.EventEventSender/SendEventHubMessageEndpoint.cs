89344e2928c717da82b6d51beae5cda96abd5cbe
Scott Rutz <scrutz@microsoft.com>
2021-09-22 10:48:04 -0500

    cp

--- Demo.EventProcessor/Demo.EventEventSender/SendEventHubMessageEndpoint.cs
+++ Demo.EventProcessor/Demo.EventEventSender/SendEventHubMessageEndpoint.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Demo.EventProcessor
+namespace Demo.EventEventSender
 {
     public static class SendEventHubMessageEndpoint
     {
--- Demo.EventProcessor/Demo.EventProcessor/EventProcessorActivity.cs
+++ Demo.EventProcessor/Demo.EventProcessor/EventProcessorActivity.cs
@@ -15,16 +15,14 @@ namespace Demo.EventProcessor
         private static List<string> ignoreTags = new List<string>() { "Tag5", "Tag6" };
 
         [FunctionName("EventProcessorActivity")]
-        public static async Task Run([EventHubTrigger("ingest-001", Connection = "IngestEventHubConnectionString")] EventData[] events,
-                                     [EventHub("output-001", Connection = "OutputEventHubConnectionString")] IAsyncCollector<string> outputEvents, ILogger log)
+        public static async Task Run([EventHubTrigger("ingest-001", Connection = "IngestEventHubConnectionString")] EventData[] events, ILogger log)
         {
             log.LogMetric("EventProcessorActivityBatchSize", events.Count(), new Dictionary<string, object> { { "RunId", Guid.NewGuid()} });
             foreach (EventData eventData in events)
             {
                 var messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
                 var telemetryModel = JsonSerializer.Deserialize<TelemetryModel>(messageBody);
-                var telemetryModelFiltered = new TelemetryModel() { Data = telemetryModel.Data.Where(t => !ignoreTags.Any(i => i == t.Name)) };
-                await outputEvents.AddAsync(JsonSerializer.Serialize(telemetryModelFiltered));
+                log.LogDebug("Event Data", eventData);
             }
         }
     }
