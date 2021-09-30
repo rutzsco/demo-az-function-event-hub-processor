5ca2c957b925996df8ecb34d1882cf7f4c920e95
Scott Rutz <scrutz@microsoft.com>
2021-09-30 14:42:00 -0500

    cp

--- Demo.EventProcessor/Demo.EventSender/SendEventHubMessageEndpoint.cs
+++ Demo.EventProcessor/Demo.EventSender/SendEventHubMessageEndpoint.cs
@@ -7,6 +7,8 @@ using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using Demo.EventSender.Model;
+using System.Diagnostics;
 
 namespace Demo.EventEventSender
 {
@@ -29,6 +31,36 @@ namespace Demo.EventEventSender
             log.LogInformation("C# HTTP trigger function processed a request.");
             string requestBody = await new StreamReader(req.Body).ReadToEndAsync();
             await outputEvents002.AddAsync(requestBody);
+            return new OkObjectResult("OK");
+        }
+
+        [FunctionName("SendEventHubMessageScenarioEndpoint")]
+        public static async Task<IActionResult> RunWithParamaters([HttpTrigger(AuthorizationLevel.Anonymous, "post", Route = null)] HttpRequest req,
+            [EventHub("ingest-002", Connection = "IngestEventHubConnectionString")] IAsyncCollector<string> outputEvents002, ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            var requestBody = await new StreamReader(req.Body).ReadToEndAsync();
+            var scenario = JsonConvert.DeserializeObject<Scenario>(requestBody);
+
+            Stopwatch durationSW = new Stopwatch();
+            durationSW.Start();
+            while (durationSW.Elapsed < TimeSpan.FromSeconds(scenario.DurationSeconds))
+            {
+                Stopwatch rateSW = new Stopwatch();
+                rateSW.Start();
+                while (durationSW.Elapsed < TimeSpan.FromSeconds(1))
+                {
+                    for (int x = 0; x < scenario.RatePerSeconds; x++)
+                    {
+                        await outputEvents002.AddAsync(requestBody);
+                    }          
+                }
+                rateSW.Stop();
+                rateSW.Reset();
+            }
+            durationSW.Stop();
+
             return new OkObjectResult("OK");
         }
     }
