0bc8636f8a03e44901e883fb1e949c07a5c4790f
Scott Rutz <scrutz@microsoft.com>
2021-10-04 11:13:12 -0500

    cp

--- Demo.EventProcessor/Demo.EventProcessor/EventProcessorActivity.cs
+++ Demo.EventProcessor/Demo.EventProcessor/EventProcessorActivity.cs
@@ -6,6 +6,7 @@ using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.EventHubs;
+using Microsoft.Azure.EventHubs.Processor;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
 
@@ -16,33 +17,39 @@ namespace Demo.EventProcessor
         private static List<string> ignoreTags = new List<string>() { "Tag5", "Tag6" };
 
         [FunctionName("EventProcessorActivity")]
-        public static async Task Run([EventHubTrigger("ingest-001", Connection = "IngestEventHubConnectionString")] EventData[] events, ILogger log)
+        public static async Task Run([EventHubTrigger("ingest-001", Connection = "IngestEventHubConnectionString")] EventData[] events, ILogger log, PartitionContext context)
         {
-            log.LogMetric("EventProcessorActivityBatchSize", events.Count(), new Dictionary<string, object> { { "RunId", Guid.NewGuid()} });
+            log.LogMetric("EventProcessorActivityBatchSize", events.Count(), new Dictionary<string, object> { { "PartitionId", context.PartitionId } });
             foreach (EventData eventData in events)
             {
                 var messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
                 var telemetryModel = JsonSerializer.Deserialize<TelemetryModel>(messageBody);
-                LogDiagnostics(eventData, messageBody, log);
+                LogDiagnostics(eventData, messageBody, log, context);
                 Thread.Sleep(telemetryModel.DelayMS);
             }
         }
 
         [FunctionName("EventProcessorActivity002")]
-        public static async Task Run2([EventHubTrigger("ingest-002", Connection = "IngestEventHubConnectionString")] EventData[] events, ILogger log)
+        public static async Task Run2([EventHubTrigger("ingest-002", Connection = "IngestEventHubConnectionString")] EventData[] events, ILogger log, PartitionContext context)
         {
             log.LogMetric("EventProcessorActivityBatchSize", events.Count(), new Dictionary<string, object> { { "RunId", Guid.NewGuid() } });
             foreach (EventData eventData in events)
             {
                 var messageBody = Encoding.UTF8.GetString(eventData.Body.Array, eventData.Body.Offset, eventData.Body.Count);
                 var telemetryModel = JsonSerializer.Deserialize<TelemetryModel>(messageBody);
-                LogDiagnostics(eventData, messageBody, log);
+                LogDiagnostics(eventData, messageBody, log, context);
                 Thread.Sleep(telemetryModel.DelayMS);
             }
         }
 
-        private static void LogDiagnostics(EventData eventData, string messageBodyString, ILogger log)
+ 
+        private static void LogDiagnostics(EventData eventData, string messageBodyString, ILogger log, PartitionContext context)
         {
+            var messageSequence = eventData.SystemProperties.SequenceNumber;
+            var lastEnqueuedSequence = context.RuntimeInformation.LastSequenceNumber;
+            var sequenceDifference = lastEnqueuedSequence - messageSequence;
+            log.LogMetric("EventProcessorActivityPartitionSequenceLag", sequenceDifference, new Dictionary<string, object> { { "PartitionId", context.PartitionId } });
+
             var sb = new StringBuilder();    
             foreach (var properties in eventData.Properties)
             {
@@ -61,8 +68,8 @@ namespace Demo.EventProcessor
                 sb.Append(properties.Value);
                 sb.Append('|');
             }
-            log.LogInformation($"EventHubIngestionProcessor MessagePayload:  {messageBodyString}");
-            log.LogInformation($"EventHubIngestionProcessor MessageProperties:  {sb}");
+            log.LogDebug($"EventHubIngestionProcessor MessagePayload:  {messageBodyString}");
+            log.LogDebug($"EventHubIngestionProcessor MessageProperties:  {sb}");
         }
     }
 }
