f2417cebfb97f5d02bf2bf2c73a0c30135aaf00c
Scott Rutz <scrutz@microsoft.com>
2022-12-07 14:16:03 -0600

    cp

---
+++ Demo.EventProcessor/Demo.Dapr.EventProcessor.Api/Controllers/TelemetryController.cs
@@ -0,0 +1,30 @@
+﻿using Demo.EventProcessor;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Demo.Dapr.EventProcessor.Api.Controllers
+{
+    [ApiController]
+    public class TelemetryController : Controller
+    {
+        private readonly ILogger<TelemetryController> _logger;
+
+        public TelemetryController(ILogger<TelemetryController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet("status")]
+        public IActionResult Get()
+        {
+            _logger.LogInformation("Processing reqest - status");
+            return new OkObjectResult("OK");
+        }
+
+        [HttpPost("ProcessTelemetry")]
+        public async Task<ActionResult> ProcessTelemetry(TelemetryModel telemetryModel)
+        {
+            Logic.Execute(telemetryModel);
+            return Ok();
+        }
+    }
+}
